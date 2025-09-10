822ded51d15797b928a55e17a2856e94402e6eab
beffiom <beffiom@protonmail.com>
2025-09-09 22:11:39 -0400

    misc spotless differences

--- api/src/main/java/com/reliaquest/api/model/MockEmployee.java
+++ api/src/main/java/com/reliaquest/api/model/MockEmployee.java
@@ -13,19 +13,19 @@ import lombok.NoArgsConstructor;
 public class MockEmployee {
 
     private String id;
-    
+
     @JsonProperty("employee_name")
     private String employeeName;
-    
+
     @JsonProperty("employee_salary")
     private Integer employeeSalary;
-    
+
     @JsonProperty("employee_age")
     private Integer employeeAge;
-    
+
     @JsonProperty("employee_title")
     private String employeeTitle;
-    
+
     @JsonProperty("employee_email")
     private String employeeEmail;
 }
